228cd74d13b8626adc9a278726da06d606b471c6
David S. Jackson <deepbsd@yahoo.com>
2018-06-04 15:32:06 -0400

    added more stuff

--- B11_WhatAboutAsynchronousCallbacks/app.js
+++ B11_WhatAboutAsynchronousCallbacks/app.js
@@ -1,6 +1,7 @@
 // long running function
 function waitThreeSeconds() {
     // This function takes longer than 3 seconds!!!
+    // This takes more like 15 seconds to execute.
     var ms = 3000 + new Date().getTime();
     while (new Date() < ms){
         // I wanted to log each loop...
@@ -10,8 +11,11 @@ function waitThreeSeconds() {
 }
 
 function setATimeOut() {
+    // This delay needs to be long enough to notice that after
+    // waiting for 'waitThreeSeconds()' to finish, this function
+    // then starts.  It doesn't run along side of waitThreeSeconds().
     console.log("Starting setATimeOut()...");
-    var msDelay = 3000;
+    var msDelay = 15000;
     setTimeout(function() {
         console.log("Executed inside SetATimeOut() in ms: ", msDelay);
         console.log("Completed setATimeOut().");
@@ -28,6 +32,10 @@ document.addEventListener('click', clickHandler);
 
 
 waitThreeSeconds();
+
 // added this call--want to see if both types of function execute for similar durations...
+// it turns out that this executes immediately, because the "waitThreeSeconds()" function takes nearly 
+// a full minute.  I guess this executes during that time?
 setATimeOut();
+
 console.log('finished execution');
