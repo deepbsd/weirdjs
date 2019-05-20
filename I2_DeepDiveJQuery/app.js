fa944dc05520acde7e62da4fffe2a96f1c3afb3c
David S. Jackson <deepbsd@yahoo.com>
2019-05-19 21:01:32 -0400

    experimenting with some more built-in jQuery functions

--- I2_DeepDiveJQuery/app.js
+++ I2_DeepDiveJQuery/app.js
@@ -1,8 +1,10 @@
 var q = $("ul.people li");
 console.log(q);
 
+///////////////////////////////////////////////////////////////////
 // These are some more built-in functions to jQuery's prototype or
 // $.fn
+///////////////////////////////////////////////////////////////////
 console.log($.isArray(q));
 let s = $.expando;
 console.log(s);
@@ -12,4 +14,5 @@ let u = $.isEmptyObject(r);
 console.log("Empty Ojbects?  t: ",t," u: ",u);
 let myArr = [r, t, u];
 console.log("is 'r' inside an array of myArr? ", $.inArray(r, myArr));
-
+// What are the parents of the people list?
+console.log("Parents of the people list: ",q.parents());
