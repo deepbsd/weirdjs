751e6eed6bfd31b5a9efcc0a2a9a71b667fdb244
David S. Jackson <deepbsd@yahoo.com>
2019-05-19 20:45:28 -0400

    playing with some $.functions after reading the source to jQuery

--- I2_DeepDiveJQuery/app.js
+++ I2_DeepDiveJQuery/app.js
@@ -1,2 +1,15 @@
 var q = $("ul.people li");
 console.log(q);
+
+// These are some more built-in functions to jQuery's prototype or
+// $.fn
+console.log($.isArray(q));
+let s = $.expando;
+console.log(s);
+let r = {};
+let t = $.isEmptyObject(q);
+let u = $.isEmptyObject(r);
+console.log("Empty Ojbects?  t: ",t," u: ",u);
+let myArr = [r, t, u];
+console.log("is 'r' inside an array of myArr? ", $.inArray(r, myArr));
+
